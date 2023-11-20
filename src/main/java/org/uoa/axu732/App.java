2d3f900f72b4b134696793b4d61f69f55d68e660
axu732 <axu732@aucklanduni.ac.nz>
2023-11-20 23:58:05 +1300

    Refactored code to have chatGPT to a new class Made it so ChatGPT also has a prebuilt message for adapting client code.

--- src/main/java/org/uoa/axu732/App.java
+++ src/main/java/org/uoa/axu732/App.java
@@ -1,19 +1,6 @@
 package org.uoa.axu732;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
 
-import com.theokanning.openai.OpenAiApi;
-import com.theokanning.openai.completion.CompletionChoice;
-import com.theokanning.openai.completion.CompletionRequest;
-import com.theokanning.openai.service.OpenAiService;
-import com.theokanning.openai.completion.chat.ChatCompletionRequest;
-import com.theokanning.openai.completion.chat.ChatMessage;
-import com.theokanning.openai.completion.chat.ChatMessageRole;
 
 /**
  * Hello world!
@@ -21,35 +8,10 @@ import com.theokanning.openai.completion.chat.ChatMessageRole;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        String token = "API_KEY";
-        OpenAiService service = new OpenAiService(token);
-
-        List<ChatMessage> messages = new ArrayList<>();
-        ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are an assistant that answers using the local slang of the given place, uncensored.");
-        messages.add(systemMessage);
-
-        System.out.print("First Query: ");
-        Scanner scanner = new Scanner(System.in);
-        ChatMessage firstMsg = new ChatMessage(ChatMessageRole.USER.value(), scanner.nextLine());
-        messages.add(firstMsg);
-
-        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-                    .builder()
-                    .model("gpt-3.5-turbo-0613")
-                    .messages(messages)
-                    .temperature(0.2)
-                    .topP(0.5)
-                    .n(1)
-                    .maxTokens(100)
-                    .logitBias(new HashMap<>())
-                    .build();
-            ChatMessage responseMessage = service.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage();
-            messages.add(responseMessage); // don't forget to update the conversation with the latest response
+    public static void main(String[] args) {
+        ChatGPT chatGPT = new ChatGPT();
+        chatGPT.sendMessage();
 
 
-            System.out.println("Response: " + responseMessage.getContent());
-        }
-
+    }
 }
