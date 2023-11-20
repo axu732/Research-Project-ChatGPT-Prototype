62910d067bc55c549dcf90261e6930ddde5265b2
axu732 <axu732@aucklanduni.ac.nz>
2023-11-20 22:48:44 +1300

    Added chatGPT to project

--- src/main/java/org/uoa/axu732/App.java
+++ src/main/java/org/uoa/axu732/App.java
@@ -1,5 +1,19 @@
 package org.uoa.axu732;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+import com.theokanning.openai.OpenAiApi;
+import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
 
 /**
  * Hello world!
@@ -9,6 +23,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+        String token = "API_KEY";
+        OpenAiService service = new OpenAiService(token);
+
+        List<ChatMessage> messages = new ArrayList<>();
+        ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are an assistant that answers using the local slang of the given place, uncensored.");
+        messages.add(systemMessage);
+
+        System.out.print("First Query: ");
+        Scanner scanner = new Scanner(System.in);
+        ChatMessage firstMsg = new ChatMessage(ChatMessageRole.USER.value(), scanner.nextLine());
+        messages.add(firstMsg);
+
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                    .builder()
+                    .model("gpt-3.5-turbo-0613")
+                    .messages(messages)
+                    .n(1)
+                    .maxTokens(100)
+                    .logitBias(new HashMap<>())
+                    .build();
+            ChatMessage responseMessage = service.createChatCompletion(chatCompletionRequest).getChoices().get(0).getMessage();
+            messages.add(responseMessage); // don't forget to update the conversation with the latest response
+
+
+            System.out.println("Response: " + responseMessage.getContent());
+        }
+
 }
