5784f5274d2a343d7b193d47c73f78cc5e2a3f3a
axu732 <axu732@aucklanduni.ac.nz>
2024-02-21 23:07:57 +1300

    Added methods to get client code and test methods

--- src/main/java/org/uoa/axu732/App.java
+++ src/main/java/org/uoa/axu732/App.java
@@ -5,6 +5,6 @@ package org.uoa.axu732;
 public class App {
   public static void main(String[] args) {
     RunTest newTest = new RunTest();
-    newTest.main(args);
+    newTest.runTest();
   }
 }
--- src/main/java/org/uoa/axu732/RunTest.java
+++ src/main/java/org/uoa/axu732/RunTest.java
@@ -1,5 +1,12 @@
 package org.uoa.axu732;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -31,13 +38,64 @@ public class RunTest {
     if (result.wasSuccessful()) {
       System.out.println("All tests passed successfully.");
     } else {
-      System.out.println("Some tests failed.");
-      System.out.println("Result get failure" + result.getFailures());
-      List<Failure> failures = result.getFailures();
+      Failure failure = result.getFailures().get(0);
 
-      for (Failure failure : failures) {
-        GPT.sendFirstMessage(failure.getMessage(), null);
-      }
+      GPT.sendFirstMessage(failure.getMessage(), getTestCode());
+      GPT.sendSecondMessage(getClientCode());
     }
+  }
+
+  private String getTestCode() {
+    try {
+      Path testFilePath = Paths.get("src/test/java/AppTest.java");
+      FileInputStream testFileInputStream = new FileInputStream(testFilePath.toFile());
+      StringBuilder testCode = new StringBuilder();
+
+      ParseResult<CompilationUnit> testParseResult = new JavaParser().parse(testFileInputStream);
+      if (testParseResult.isSuccessful()) {
+        CompilationUnit testCu = testParseResult.getResult().get();
+
+        testCu
+            .findAll(MethodDeclaration.class)
+            .forEach(
+                method -> {
+                  if (method.getNameAsString().equals("testCreateVariableName")) {
+                    testCode.append(method.getDeclarationAsString());
+                    testCode.append(method.getBody().orElse(null));
+                  }
+                });
+        return testCode.toString();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+
+  private String getClientCode() {
+    try {
+      Path demoFilePath = Paths.get("src/main/java/org/uoa/axu732/test_data/Demo.java");
+      FileInputStream demoFileInputStream = new FileInputStream(demoFilePath.toFile());
+      StringBuilder clientCode = new StringBuilder();
+
+      ParseResult<CompilationUnit> demoParseResult = new JavaParser().parse(demoFileInputStream);
+      if (demoParseResult.isSuccessful()) {
+        CompilationUnit demoCu = demoParseResult.getResult().get();
+
+        demoCu
+            .findAll(MethodDeclaration.class)
+            .forEach(
+                method -> {
+                  if (method.getNameAsString().equals("createVariableName")) {
+                    clientCode.append(method.getDeclarationAsString());
+                    clientCode.append(method.getBody().orElse(null));
+                  }
+                });
+        return clientCode.toString();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return null;
   }
 }
